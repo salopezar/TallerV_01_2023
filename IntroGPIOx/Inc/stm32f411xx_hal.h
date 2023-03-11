6963e8460ce31540eb3770242bf0068f02f069fa
salopezar <salopezar@unal.edu.co>
2023-03-11 16:26:55 -0500

    Correcciones de los Drivers de Clase y algunas cositas variadas de la semana 5 del curso.
    Códigos extraidos de los videos del profesor Nerio y algunas cosas hechas en el taller. Todo esto pertenece a la semana 5 del curso.

--- IntroGPIOx/Inc/stm32f411xx_hal.h
+++ IntroGPIOx/Inc/stm32f411xx_hal.h
@@ -5,29 +5,78 @@
  *      Author: santiago
  */
 
+/*  Este archivo contiene la información básica del micro:
+ *   - Valores del reloj principal
+ *   - Distribución básica de la memoria (descrito en la figura 14 de la hoja de datos del micro)
+ *   - Posiciones de memoria de los perifericos disponibles en el micro descrito en la
+ *     Tabla 1 (memory map)
+ *   - Incluye los demás registros de los periféricos
+ *   - Definiciones de las constantes más básicas
+ *
+ *  NOTA: La definición del NVIC será realizada al momento de describir el uso de las
+ *  	  interrupciones
+ */
+
 #ifndef STM32F411XX_HAL_H_
 #define STM32F411XX_HAL_H_
 
 #include<stdint.h>
 #include<stddef.h>
 
-#define HSI_CLOCK_SPEED		16000000
-#define HSE_CLOCK_SPEED  	4000000
+#define HSI_CLOCK_SPEED		16000000		//Value for the main clock signal (HSI -> High Speed Internal)
+#define HSE_CLOCK_SPEED  	4000000			//Value for the main clock signal (HSE -> High Speed External)
 
+/*
+ * Base addresses of Flash and SRAM memories
+ * Datasheet, Memory Map, Figure 14
+ * (remenber, 1KByte = 1024 bytes)
+ */
 #define NOP()		asm("NOP")
 #define __weak		__attribute__((weak))
 
-#define FLASH_BASE_ADDR			0x08000000U
-#define SRAM_BASE_ADDR			0x20000000U
+#define FLASH_BASE_ADDR			0x08000000U		//Esta es la memoria del programa, 512 KB
+#define SRAM_BASE_ADDR			0x20000000U		//Esta es la memoria RAM, 128KB
 
+/*
+ * NOTA: Observar que existen unos registros específicos del Cortex M4 en la región 0xE0000000U
+ * Los controladores de las interruciones se encuentran allí.
+ */
+
+/*
+ * NOTA:
+ * Ahora agregamos la dirección de memoria base para cada uno de los periféricos que posee el micro
+ * En el "datasheet" del micro, figura 14 (Memory map) encontramos el mapa de los buses:
+ * 	- APB1 (Advance Peripheral bus)
+ * 	- APB2
+ * 	- AHB1 (Advance High-performance Bus)
+ * 	- AHB2
+ */
+
+/*
+ * AHBx and APBx Bus Peripherals base addresses
+ */
 
 #define APB1_BASE_ADDR			0x40000000U
 #define APB2_BASE_ADDR			0x40010000U
 #define AHB1_BASE_ADDR			0x40020000U
 #define AHB2_BASE_ADDR 			0x50000000U
 
+/*
+ * Y ahora debemos hacer lo mismo pero cada una de las posiciones de memoria de cada uno de los
+ * periféricos descritos en la tabla 1 del manual de referencia del micro.
+ * Observe que en dicha tabla está a su vez dividida en cuatro segmentos, cada uno correspondiente
+ * a APB1, APB2, AHB1, AHB2.
+ *
+ * Comenzar de arriba hacia abajo como se muestra en la tabla. Inicia USB_OTG_FS (AHB2)
+ *
+ * NOTA: Solo lo vamos a hacer con los elementos que necesitamos en este ejercicio: RCC Y GPIOx
+ */
+
+/*Posiciones de memoria para periféricos del AHB2 */
 #define USB_OTG_FS_ADDR		(AHB2_BASE_ADDR + 0x3800U)
 
+/*Posiciones de memoria para periféricos del AHB1
+ * Observar que NO está completa*/
 #define RCC_BASE_ADDR 		(AHB1_BASE_ADDR + 0x3800U)
 #define GPIOH_BASE_ADDR		(AHB1_BASE_ADDR + 0x1C00U)
 #define GPIOE_BASE_ADDR 	(AHB1_BASE_ADDR + 0x1000U)
@@ -36,7 +85,9 @@
 #define GPIOB_BASE_ADDR 	(AHB1_BASE_ADDR + 0x0400U)
 #define GPIOA_BASE_ADDR 	(AHB1_BASE_ADDR + 0x0000U)
 
-
+/**
+ * Macros Genéricos
+ */
 #define ENABLE 			1
 #define DISABLE 		0
 #define SET				ENABLE
@@ -49,7 +100,17 @@
 #define I2C_WRITE		0
 #define I2C_READ		1
 
+/* +++======= INICIO de la descripción de los elementos que componen el periférico =======+++ */
 
+/* Definición de la estructura de datos que representa a cada uno de los registros que componen el
+ * el periférico RCC.
+ *
+ * Debido a los temas que se van a manejar en el curso, solo se deben definir los bits de los registros:
+ * 6.3.1 (RCC_CR) hasta el 6.3.12 (RCC_APB2ENR), 6.3.17 (RCC_BDCR) Y 6.3.18 (RCC_CSR)
+ *
+ * NOTA: La posición de memoria (offset) debe encajar perfectamente con la posición de memoria indicada
+ * en la hoja de datos de equipo. Observe que los elementos "reservedx" también están presentes allí.
+ * */
 typedef struct
 {
 	volatile uint32_t CR;
@@ -91,9 +152,15 @@ typedef struct
 
 } RCC_RegDef_t;
 
+/*
+ * Como se vio en la clase de introducción a las estructuras, hacemos un puntero a RCC_RegDef_t que
+ * apunta a la posición exacta del periférico RCC, de forma que cada miembro de la estructura coincide
+ * con cada uno de los SFR en la memoria del MCU. Esta acción la estamos haciendo en un MACRO, de forma
+ * que el nuevo elemento "RCC" queda disponible para cada clase .c (archivo .c) que incluya este archivo
+ * */
 #define RCC			((RCC_RegDef_t *) RCC_BASE_ADDR)
 
-
+/* 6.3.9 RCC_AHB1ENR */
 #define RCC_AHB1ENR_GPIOA_EN		0
 #define RCC_AHB1ENR_GPIOB_EN		1
 #define RCC_AHB1ENR_GPIOC_EN		2
@@ -104,6 +171,15 @@ typedef struct
 #define RCC_AHB1ENR_DMA1_EN			21
 #define RCC_AHB1ENR_DMA2_EN			22
 
+/* ======= FIN de la descripción de los elementos que componen el periférico RCC ======= */
+
+/* +++==== INICIO de la descripción de los elementos que componen el periférico GPIOx ====+++ */
+/* Definición de la estructura de datos que representa a cada uno de los registros que componen el
+ * periférico GPIO.
+ * Debido a que el periférico GPIOx es muy simple, no es muy necesario crear la descripción bit a bit
+ * de cada uno de los registros que componen el dicho periférico, pero si es necesario comprender qué
+ * hace cada registro, para poder cargar correctamente la configuración.
+ */
 typedef struct
 {
 	volatile uint32_t MODER;
@@ -119,6 +195,13 @@ typedef struct
 
 }GPIOx_RegDef_t;
 
+/* Al igual que con el RCC, creamos un puntero a la estructura que define a GPIOx y debemos hacer
+ * que cada GPIOx (A, B, C...) quede ubicado exactamente sobre la posición de memoria correcta.
+ * Debido a que son varios periféricos GPIOx, es necesario hacer la definición para cada uno.
+ *
+ * NOTA: tener cuidado que cada elemento coincida con su respectiva dirección base.
+ * */
+
 #define GPIOA			((GPIOx_RegDef_t *) GPIOA_BASE_ADDR)
 #define GPIOB			((GPIOx_RegDef_t *) GPIOB_BASE_ADDR)
 #define GPIOC			((GPIOx_RegDef_t *) GPIOC_BASE_ADDR)
@@ -127,27 +210,36 @@ typedef struct
 #define GPIOH			((GPIOx_RegDef_t *) GPIOH_BASE_ADDR)
 
 
+/* valores estándar para las configuraciones */
+/* 8.4.1 GPIOx_MODER (dos bit por cada PIN) */
 #define GPIO_MODE_IN		0
 #define GPIO_MODE_OUT		1
 #define GPIO_MODE_ALTFN		2
 #define GPIO_MODE_ANALOG	3
 
+/* 8.4.2 GPIOx_OTYPER (un bit por cada PIN) */
 #define GPIO_OTYPE_PUSHPULL		0
 #define GPIO_OTYPE_OPENDRAIN	1
 
-
+/* 8.4.3 GPIOx_OSPEEDR (dos bit por cada PIN) */
 #define GPIO_OSPEED_LOW			0
 #define GPIO_OSPEED_MEDIUM		1
 #define GPIO_OSPEED_FAST		2
 #define GPIO_OSPEED_HIGH     	3
 
-
+/* 8.4.4 GPIOx_PUPDR (dos bit por cada PIN) */
 #define GPIO_PUPDR_NOTHING		0
 #define GPIO_PUPDR_PULLUP		1
 #define GPIO_PUPDR_PULLDOWN		2
 #define GPIO_PUPDR_RESERVED		3
 
+/* 8.4.5 GPIOx_IDR (un bit por cada PIN) - este es el registro para leer el estado de un PIN */
 
+/* 8.4.6 GPIOx_ODR (un bit por cada PIN) - este es el registro para escribir el estado de un
+ * PIN (1 o 0). Este registro puede ser escrito y leído desde el software, pero no garantiza
+ * una estructura "atómica", por lo cual es preferible utilizar el registro BSRR */
+
+/* Definición de los nombres de los pines */
 #define PIN_0		0
 #define PIN_1		1
 #define PIN_2		2
@@ -166,6 +258,7 @@ typedef struct
 #define PIN_15		15
 
 
+/* Definición de las funciones alternativas */
 #define AF0		0b0000
 #define AF1		0b0001
 #define AF2		0b0010
@@ -183,4 +276,11 @@ typedef struct
 #define AF14	0b1110
 #define AF15	0b1111
 
+/*
+ typedef struct
+ {
+ 	 volatile uint32_t dummy;		//	Dummy Example register				ADDR_OFFSET:	0x00
+ } DUMMY_RegDef_t;
+ */
+/* No es necesario hacer nada más en este archivo, para este primer proyecto */
 #endif /* STM32F411XX_HAL_H_ */
